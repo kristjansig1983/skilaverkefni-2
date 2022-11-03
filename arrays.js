f0bf3caae0e2cfc3eed8e57da36e57f55bb0fbb9
kristjansig1983 <91916422+kristjansig1983@users.noreply.github.com>
2022-11-03 20:45:12 +0000

    First four done
    Finished first four problems in basics.

---
+++ arrays.js
@@ -0,0 +1,123 @@
+// It should return the last n elements of a
+// Tip: use the array prototype function slice()
+const removeLastNElements = (a, n) => {
+    return;
+  };
+  
+  /*
+  Test cases:
+  myFunction([1, 2, 3, 4, 5], 2) Expected [ 4, 5 ]
+  myFunction([1, 2, 3], 6) Expected [ 1, 2, 3 ]
+  myFunction([1, 2, 3, 4, 5, 6, 7, 8], 3) Expected [ 6, 7, 8 ]
+  */
+  
+  // ========================================
+  // Write a function that takes an array (a) as argument
+  // Return the number of elements in a
+  // Tip: How do you find the length of an array?
+  const countNumberOfElements = (a) => {
+    return;
+  };
+  /*
+  Test cases: 
+  myFunction([1,2,2,4]) Expected 4
+  myFunction([9,9,9]) Expected 3
+  myFunction([4,3,2,1,0]) Expected 5
+  */
+  
+  // ========================================
+  
+  // Write a function that takes an array of numbers as argument
+  // Return the number of negative values in the array
+  // Tip: There are multiple ways to solve this e.g.
+  // - Use .filter() to filter only negative numbers, and count them
+  // - Use .forEach() to iterate through all numbers and count negatives
+  const countNumberOfNegativeValues = (a) => {
+    return;
+  };
+  
+  /*
+  Test cases:
+  myFunction([1,-2,2,-4]) Expected 2
+  myFunction([0,9,1]) Expected 0
+  myFunction([4,-3,2,1,0]) Expected 1
+  */
+  
+  // ========================================
+  
+  // Write a function that takes an array of numbers as argument
+  // It should return the sum of the numbers
+  // Tip: forEach, c-style loop (or even .reduce() for the brave)
+  const calcSumOfArrayOfNumbers = (a) => {
+    return;
+  };
+  
+  /*
+  Test cases:
+  myFunction([10,100,40]) Expected 150
+  myFunction([10,100,1000,1]) Expected 1111
+  myFunction([-50,0,50,200]) Expected 200
+  */
+  
+  // ========================================
+  // Write a function that takes an array of numbers as argument
+  // It should return the average of the numbers
+  // Tip: forEach, c-style loop (or even .reduce() for the brave)
+  // to get the sum, then divide by number of elements in array
+  const calcAvgOfArrayOfNumbers = (arr) => {
+    return;
+  };
+  
+  /*
+  Test cases:
+  myFunction([10,100,40]) Expected 50
+  myFunction([10,100,1000]) Expected 370
+  myFunction([-50,0,50,200]) Expected 50
+  */
+  
+  // ========================================
+  // Write a function that takes an array of strings as argument
+  // Return the longest string
+  // Tip: It's possible to get length of string with .length
+  // E.g. 'Gunnsteinn'.length = 10
+  const getLongestStringFromArray = (arr) => {
+    return;
+  };
+  
+  /*
+  Test cases:
+  myFunction(['help', 'me']) Expected 'help'
+  myFunction(['I', 'need', 'candy']) Expected 'candy'
+  */
+  
+  // ========================================
+  // Write a function that takes an array as argument
+  // It should return true if all elements in the array are equal
+  // It should return false otherwise
+  // Tip: STRICTLY equal, ====
+  const areAllEqual = (arr) => {
+    return;
+  };
+  
+  /*
+  Test cases:
+  myFunction([true, true, true, true]) Expected true 
+  myFunction(['test', 'test', 'test']) Expected true 
+  myFunction([1,1,1,2]) Expected false 
+  myFunction(['10',10,10,10]) Expected false 
+  */
+  // ========================================
+  // Write a function that takes arguments an arbitrary number of arrays
+  // It should return an array containing the values of all arrays
+  // Tip: Make use of the spread syntax (...), as the parameters suggest
+  const mergeAllArrays = (...arrays) => {
+    return;
+  };
+  
+  /*
+  Test cases:
+  myFunction([1, 2, 3], [4, 5, 6]) Expected [1, 2, 3, 4, 5, 6]
+  myFunction(['a', 'b', 'c'], [4, 5, 6]) Expected ['a', 'b', 'c', 4, 5, 6]
+  myFunction([true, true], [1, 2], ['a', 'b']) Expected [true, true, 1, 2, 'a', 'b']
+  */
+  
---
+++ basics.js
@@ -0,0 +1,176 @@
+// Write a function that takes two numbers (a and b) as argument
+//  Sum a and b
+// Return the result
+
+// Done
+const calcSum = (a, b) => {
+    return a + b;
+  };
+  
+  
+  
+  /*
+  Test cases:
+  calcSum(1,2) Expected 3
+  calcSum(1,10) Expected 11
+  calcSum(99,1) Expected 100
+  */
+  
+  // ========================================
+  
+  // Write a function that takes two values, say a and b, as arguments
+  // Return true if the two values are equal and of the same type
+  // Done
+  const strictEquality = (a, b) => {
+    return a === b;
+  };
+  
+  /*
+  Test cases:
+  myFunction(2, 3) Expected false
+  myFunction(3, 3) Expected true
+  myFunction(1, '1') Expected false
+  myFunction('10', '10') Expected true 
+  */
+  
+  // ========================================
+  
+  // Write a function that takes a value as argument
+  // Return the type of the value
+  //Done
+  const getTypeOfValue = (a) => {
+    return typeof a;
+  };
+  
+  /*
+  Test cases:
+  myFunction(1) Expected 'number'
+  myFunction(false) Expected 'boolean'
+  myFunction({}) Expected 'object'
+  myFunction(null) Expected 'object'
+  myFunction('string') Expected 'string'
+  myFunction(['array']) Expected 'object'
+  */
+  
+  // ========================================
+  
+  // Write a function that takes a string (a) as argument
+  // Return the 1st character of the strgin a
+  // Tip: look up the string prototype function slice() or split()
+  // Done
+  const getFirstChar = (a, n) => {
+    return a.slice(0,1);
+  };
+  
+  /*
+  Test Cases:
+  myFunction('abcd') Expected 'a'
+  myFunction('zyxbwpl') Expected 'z'
+  myFunction('gfedcba') Expected 'g'
+  */
+  
+  // ========================================
+  
+  // Write a function that takes a string (a) and a number (n) as argument
+  // Return the nth character of 'a'
+  // Tip: look up the string prototype function slice() or split()
+  const getNthChar = (a, n) => {
+    return n.slice();
+  };
+  
+  /*
+  Test Cases:
+  myFunction('abcd',1) Expected 'a'
+  myFunction('zyxbwpl',5) Expected 'w'
+  myFunction('gfedcba',3) Expected 'e'
+  */
+  
+  // ========================================
+  
+  // Write a function that takes a string (a) as argument
+  // Extract the first half a
+  // Return the result
+  // Tip: look up the string prototype function slice() or split()
+  const extractFirstHalfOfString = (a) => {
+    return;
+  };
+  
+  /*
+  Test Cases:
+  myFunction('abcdefgh') Expected 'abcd'
+  myFunction('1234') Expected '12'
+  myFunction('gedcba') Expected 'ged'
+  */
+  
+  // ========================================
+  
+  // Write a function that takes a string (a) as argument
+  // Remove the last 3 characters of a
+  // Return the result
+  const removeLastNChractersOfString = (a) => {
+    return;
+  };
+  
+  /*
+  Test cases:
+  myFunction('abcdefg') Expected 'abcd'
+  myFunction('1234') Expected '1'
+  myFunction('fgedcba') Expected 'fged'
+  */
+  
+  // ========================================
+  
+  // Write a function that takes a number as argument
+  // If the number is even, return true
+  // Otherwise, return false
+  // Tip: How does the % operator work?
+  const checkIfNumberIsEven = (a) => {
+    return;
+  };
+  
+  /*
+  Test Cases:
+  myFunction(10) Expected true
+  myFunction(-4) Expected true
+  myFunction(5) Expected false
+  myFunction(-111) Expected false
+  */
+  
+  // ========================================
+  
+  // Write a function that takes two numbers (a and b) as argument
+  // Return b percent of a
+  const getPercentageOfNumber = (a, b) => {
+    return;
+  };
+  
+  /*
+  Test cases:
+  myFunction(100,50) Expected 50
+  myFunction(10,1) Expected 0.1
+  myFunction(500,25) Expected 125
+  */
+  
+  // ========================================
+  
+  // Write a function that takes 6 values (a,b,c,d,e,f) as arguments
+  // Sum a and b
+  // Then substract by c
+  // Then multiply by d and divide by e
+  // Finally raise to the power of f and return the result
+  // Tip:
+  // - Mind the order
+  // - Power operator is either Math.pow or **
+  //   - e.g. 2**2 = 4
+  const useAllTheOperators = (a, b, c, d, e, f) => {
+    return;
+  };
+  
+  /*
+  Test cases:
+  myFunction(6,5,4,3,2,1) Expected 10.5
+  myFunction(6,2,1,4,2,3) Expected 2744
+  myFunction(2,3,6,4,2,3) Expected -8
+  */
+  
+  // ========================================
