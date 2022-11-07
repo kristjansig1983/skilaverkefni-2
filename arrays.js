3917029ed11ffa314a713ad29b6dc43771ecd874
Kristján Sigurðsson <kristjan.sigurdsson@samskip.com>
2022-11-07 14:04:29 +0000

    Forgot to save
    Forgot to save one change in last commit.

--- arrays.js
+++ arrays.js
@@ -152,6 +152,7 @@ const removeLastNElements = (a, n) => {
   // Write a function that takes arguments an arbitrary number of arrays
   // It should return an array containing the values of all arrays
   // Tip: Make use of the spread syntax (...), as the parameters suggest
+  //Done!
   const mergeAllArrays = (...arrays) => {
     return arrays.concat(...arrays);
   };
